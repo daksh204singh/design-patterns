c2dba3822b183a91d693cedb6befe746a89a85d8
Daksh Pratap Singh <daksh204singh@gmail.com>
2021-02-18 02:16:50 +0530

    removed git-init.sh -> not-relevant to the project

--- git-init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:daksh204singh/design-patterns.git
-git push -u origin main
